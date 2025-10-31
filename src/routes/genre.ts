e2e7c07762fde73ed05e8d13eac8770e14dc987b
Martin Rada <martinradadl@gmail.com>
2025-10-31 11:11:39 -0500

    get endpoints added to genre routes

--- src/routes/genre.ts
+++ src/routes/genre.ts
@@ -4,5 +4,7 @@ import * as genreController from "../controllers/genre";
 const router = express.Router();
 
 router.post("/", genreController.add);
+router.get("/:id", genreController.getById);
+router.get("/", genreController.getAll);
 
 export default router;
