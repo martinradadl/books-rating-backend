dea66de072fa8726b685c15ca61f9de863c9986b
Martin Rada <martinradadl@gmail.com>
2025-10-31 11:49:13 -0500

    name property on Setting model defined as unique

--- src/models/setting.ts
+++ src/models/setting.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-  name: String,
+  name: { String, unique: true },
 });
 
 export const Setting = mongoose.model("Setting", schema);
