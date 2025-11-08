8d79f7bd8fa457d0ad9f0240ca14c5f78c514bd2
Martin Rada <martinradadl@gmail.com>
2025-11-07 20:29:08 -0500

    models updated, required: true added to some models

--- src/models/book.ts
+++ src/models/book.ts
@@ -7,10 +7,10 @@ const schema = new mongoose.Schema({
     ref: "Author",
     required: true,
   },
-  relatedGenres: [{ type: mongoose.Types.ObjectId, ref: "Genre" }],
+  relatedGenresIds: [{ type: mongoose.Types.ObjectId, ref: "Genre" }],
   firstPublished: Date,
-  setting: [{ type: mongoose.Types.ObjectId, ref: "Setting" }],
-  characters: [{ type: mongoose.Types.ObjectId, ref: "Character" }],
+  charactersIds: [{ type: mongoose.Types.ObjectId, ref: "Character" }],
+  settingIds: [{ type: mongoose.Types.ObjectId, ref: "Setting" }],
 });
 
 schema.index({ originalTitle: 1, authorId: 1 }, { unique: true });
