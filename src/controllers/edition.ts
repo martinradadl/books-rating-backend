b3b2fd3a52da915e43172fd7948437c9a6e39411
Martin Rada <martinradadl@gmail.com>
2025-12-01 12:11:11 -0500

    Endpoints created for getMoreEditions, getBooksBySameAuthor and getRelatedBooks

--- src/controllers/edition.ts
+++ src/controllers/edition.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import * as editionModel from "../models/edition";
 import * as bookModel from "../models/book";
 import { MONGO_ERRORS } from "../helpers/constants";
+import mongoose from "mongoose";
 
 export const add = async (req: Request, res: Response) => {
   try {
@@ -72,6 +73,142 @@ export const getAll = async (req: Request, res: Response) => {
           { path: "settings" },
         ],
       });
+
+    res.status(200).json(editionsList);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+};
+
+export const getMoreEditions = async (req: Request, res: Response) => {
+  try {
+    const editionId = req.query?.editionId;
+    const bookId = req.query?.bookId;
+
+    const editionsList = await editionModel.Edition.find({
+      book: bookId,
+      _id: { $ne: new mongoose.Types.ObjectId(editionId as string) },
+    }).populate({
+      path: "book",
+      populate: [{ path: "author" }],
+    });
+
+    res.status(200).json(editionsList);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+};
+
+export const getBooksBySameAuthor = async (req: Request, res: Response) => {
+  try {
+    const authorId = req.query?.authorId;
+    const bookId = req.query?.bookId;
+
+    const editionsList = await editionModel.Edition.aggregate([
+      {
+        $lookup: {
+          from: "books",
+          localField: "book",
+          foreignField: "_id",
+          as: "book",
+        },
+      },
+      { $unwind: "$book" },
+      {
+        $lookup: {
+          from: "authors",
+          localField: "book.author",
+          foreignField: "_id",
+          as: "book.author",
+        },
+      },
+      { $unwind: "$book.author" },
+      {
+        $match: {
+          "book.author._id": new mongoose.Types.ObjectId(authorId as string),
+          "book._id": { $ne: new mongoose.Types.ObjectId(bookId as string) },
+        },
+      },
+      {
+        $group: {
+          _id: "$book",
+          edition: { $first: "$$ROOT" },
+        },
+      },
+      {
+        $replaceRoot: { newRoot: "$edition" },
+      },
+    ]);
+
+    res.status(200).json(editionsList);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+};
+
+export const getRelatedBooks = async (req: Request, res: Response) => {
+  try {
+    const authorId = req.query?.authorId;
+    const bookId = req.query?.bookId;
+
+    const baseBook = await bookModel.Book.findById(bookId)
+      .select("relatedGenres")
+      .lean();
+    const relatedGenres = baseBook?.relatedGenres;
+
+    const editionsList = await editionModel.Edition.aggregate([
+      {
+        $lookup: {
+          from: "books",
+          localField: "book",
+          foreignField: "_id",
+          as: "book",
+        },
+      },
+      { $unwind: "$book" },
+      {
+        $lookup: {
+          from: "authors",
+          localField: "book.author",
+          foreignField: "_id",
+          as: "book.author",
+        },
+      },
+      { $unwind: "$book.author" },
+      {
+        $match: {
+          "book.author._id": {
+            $ne: new mongoose.Types.ObjectId(authorId as string),
+          },
+        },
+      },
+      {
+        $addFields: {
+          genreOverlap: {
+            $size: { $setIntersection: ["$book.relatedGenres", relatedGenres] },
+          },
+        },
+      },
+      { $match: { genreOverlap: { $gt: 0 } } },
+      {
+        $sort: { genreOverlap: -1 },
+      },
+      {
+        $group: {
+          _id: "$book",
+          edition: { $first: "$$ROOT" },
+        },
+      },
+      {
+        $replaceRoot: { newRoot: "$edition" },
+      },
+    ]);
 
     res.status(200).json(editionsList);
   } catch (err: unknown) {
