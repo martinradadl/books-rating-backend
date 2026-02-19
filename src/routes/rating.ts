554d9ae98ddfaf9e27a90a18c3467ea8d9bd8f4d
Martin Rada <martinradadl@gmail.com>
2026-02-19 14:29:09 -0500

    Unnecessary Ratings endpoints removed

--- src/routes/rating.ts
+++ src/routes/rating.ts
@@ -4,7 +4,5 @@ import * as ratingController from "../controllers/rating";
 const router = express.Router();
 
 router.post("/", ratingController.add);
-router.get("/count/:bookId", ratingController.getCountByBook);
-router.get("/mean/:bookId", ratingController.getMeanRatingByBook);
 
 export default router;
