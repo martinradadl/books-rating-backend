bb078f6dd64bd3025c22267779978d4af80d11de
Martin Rada <martinradadl@gmail.com>
2026-02-27 10:36:25 -0500

    endpoint for getting rating dustribution by score added

--- src/controllers/rating.ts
+++ src/controllers/rating.ts
@@ -1,11 +1,71 @@
 import { Request, Response } from "express";
 import * as ratingModel from "../models/rating";
+import { parseToObjectId } from "../helpers/utils";
 
 export const add = async (req: Request, res: Response) => {
   try {
     const newRating = await ratingModel.Rating.create(req.body);
-
     res.status(200).json(newRating);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+};
+
+export const getRatingDistributionByScore = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const bookId = parseToObjectId(req.params.bookId);
+
+    const [ratingDistribution] = await ratingModel.Rating.aggregate([
+      {
+        $match: {
+          book: bookId,
+        },
+      },
+      {
+        $group: {
+          _id: "$score",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          total: { $sum: "$count" },
+          ratings: {
+            $push: {
+              k: { $toString: "$_id" },
+              v: "$count",
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          total: 1,
+          ratings: {
+            $mergeObjects: [
+              { "5": 0, "4": 0, "3": 0, "2": 0, "1": 0 },
+              { $arrayToObject: "$ratings" },
+            ],
+          },
+        },
+      },
+      {
+        $replaceRoot: {
+          newRoot: {
+            $mergeObjects: ["$ratings", { total: "$total" }],
+          },
+        },
+      },
+    ]);
+
+    res.status(200).json(ratingDistribution);
   } catch (err: unknown) {
     if (err instanceof Error) {
       res.status(500).json({ message: err.message });
