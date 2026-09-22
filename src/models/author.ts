9e2312aa7f99d3d043fb046e5c18210b7790e307
Martin Rada <martinradadl@gmail.com>
2026-09-22 16:22:54 -0500

    birthplace, birthdate and deathdate added to Author's Model

--- src/models/author.ts
+++ src/models/author.ts
@@ -4,6 +4,9 @@ const schema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   profilePic: String,
   description: String,
+  birthplace: String,
+  birthdate: Date,
+  deathdate: Date,
 });
 
 export const Author = mongoose.model("Author", schema);
